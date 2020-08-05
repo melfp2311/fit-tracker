23e31fba96b5970d993b3aef63c32f6e017f11d0
Melissa Flores <mel.floresp@gmail.com>
2020-08-05 09:58:33 -0500

    added routes folder

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false
 });
 
+// routes
 app.use(require("./routes/html-routes"));
 app.use(require("./routes/api-routes"));
 
